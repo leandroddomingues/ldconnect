b481a4f635c21984a6ca0ac891d0eb023862e899
leandroddomingues <95154477+leandroddomingues@users.noreply.github.com>
2021-12-02 17:44:01 -0300

    Delete EnviarParaOFacebook directory

--- EnviarParaOFacebook/index.php
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="processa.php" enctype="multipart/form-data">
-	<label>Nome: </label>
-	<input type="text" name="nome"><br><br>
-	<label>Imagem: </label>
-	<input type="file" name="arquivo"/><br><br>
-	<input type="submit" value="Cadastrar">
-</form>
